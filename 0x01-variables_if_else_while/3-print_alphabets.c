de1fd936759edfa155c2be0c2d2cb566622a02e5
Shija-cmd <shijajuma@protonmail.com>
2022-10-14 17:55:50 -0700

    lower and upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  * main - print alpha in lower case then in upper case
+  *
+  */
+int main(void)
+{
+	char c = 'a';
+	char d = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
