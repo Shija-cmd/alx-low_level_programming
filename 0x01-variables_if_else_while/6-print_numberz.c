5f3a3e33b7da8348358478ede111acb12f7112f2
Shija-cmd <shijajuma@protonmail.com>
2022-10-14 18:41:15 -0700

    add numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  * main - print numbers without using char, printf or puts but putchar can be 
+  *used twice.
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar (i);
+		i++;
+	}
+	putchar ('\n');
+	return (0);
+}
