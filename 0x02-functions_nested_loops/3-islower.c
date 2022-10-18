d70a7e1406a9d7aa8b0af15602387f5560661539
Shija-cmd <shijajuma@protonmail.com>
2022-10-18 16:36:53 -0700

    add _islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,13 @@
 #include "main.h"
 /**
   * _islower - checks the lower case letter
-  *
+  *@c: character to check
   * Return: 0 or 1
   */
+
+
+
+
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
