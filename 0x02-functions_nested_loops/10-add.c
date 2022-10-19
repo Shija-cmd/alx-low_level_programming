df4ae12fdbc477c175d1170ccd6c052bcd69f225
Shija-cmd <shijajuma@protonmail.com>
2022-10-19 08:20:40 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * add - add two integers
+  * @x: integer type number
+  * @y: integer type number
+  *
+  * Return: Sume
+  */
+int add(int x, int y)
+{
+	return (x + y);
+}
