75ff94bd008339b5d3b1e2c94f0a074fb3e1ec41
Shija-cmd <shijajuma@protonmail.com>
2022-10-19 13:49:29 -0700

    counting natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,12 +10,11 @@ void print_to_98(int n)
 {
 	while (n != 98)
 	{
-		printf ("%i, " , n);
+		printf("%i, " , n);
 		if (n > 98)
 			n--;
 		else
 			n++;
 	}
-	printf ("98\n");
+	printf("98\n");
 }
-	
