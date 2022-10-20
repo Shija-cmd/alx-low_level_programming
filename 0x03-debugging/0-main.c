eba493ded4f0fa5306a079decf2c38b9e6e0e06e
Shija-cmd <shijajuma@protonmail.com>
2022-10-20 20:57:01 +0000

    second commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,13 +2,13 @@
 /**
   * main - tests function that prints if integeris positive or negative
   * 
-  * return: 0
+  * Return: 0
   */
 int main(void)
 {
 	int i;
 
 	i = 0;
-	positive_or_negative (i);
+	positive_or_negative(i);
 	return (0);
 }
