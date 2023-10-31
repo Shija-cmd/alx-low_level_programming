d335f74b6071d0c8a3773a14fcaf572789b09760
Shija-cmd <shijajuma@protonmail.com>
2023-10-31 11:28:50 -0700

    my first commit on argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Points the name of the program
+ * @argc: number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	print("%s\n", argv[argc * 0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Argument control and argument vector
