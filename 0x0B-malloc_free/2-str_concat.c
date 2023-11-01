75ea1c2483adb6825e4b1a0ec471a3901b1f9300
Shija-cmd <shijajuma@protonmail.com>
2023-11-01 04:09:08 -0700

    another commit on malloc free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * main - str_concat - Entry point
+ * str_concat - Entry point
  * @s1: string 1
  * @s2: string 2
  * Return: pointer should point to a newly allocated space in memory or NULL
