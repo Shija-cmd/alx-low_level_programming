3abe59bc78776540166fa32e265d231c650d9ce4
Shija-cmd <shijajuma@protonmail.com>
2023-11-08 06:16:09 -0800

    another commit in array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,5 @@
 #include "function_pointers.h"
 #include <stdlib.h>
-
 /**
  * array_iterator - iterates an array
  * @array: is an array
@@ -10,10 +9,12 @@
 void array_iterator(int *array, size_t size, void(*action)(int))
 {
 	unsigned int i;
+
 	if (array == NULL)
 		return;
 	if (action == NULL)
 		return;
+
 	for (i = 0; i < size; i++)
 		action(array[i]);
 }
