1ffc73b5759646dbe2b0406fc3c0369b90f3b9e9
Shija-cmd <shijajuma@protonmail.com>
2023-11-20 22:30:41 +0000

    updated 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,6 +3,7 @@
 /**
  * free_listint2 - frees all the node in a list and reset the head to NULL.
  * @head: pointer of pointer to a list.
+ * Return: Nothing
  */
 void free_listint2(listint_t **head)
 {
@@ -21,6 +22,7 @@ void free_listint2(listint_t **head)
 /**
  * free_listint - frees all the node in a list.
  * @head: pointer to a list.
+ * Return: Nothing
  */
 void free_listint(listint_t *head)
 {
