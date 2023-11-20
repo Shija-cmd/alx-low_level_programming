8b743d033fc1c26d3c82c2ab3892009debf073cc
Shija-cmd <shijajuma@protonmail.com>
2023-11-20 22:38:50 +0000

    updated 8-sum_listint.c file

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -4,7 +4,7 @@
  * sum_listint - sum up all the value in each element of a listint_t
  * @head: pointer to the head of a list.
  *
- * Return: Sum (INT) else 0 if empty.
+ * Return: Sum else 0 if empty.
  */
 int sum_listint(listint_t *head)
 {
