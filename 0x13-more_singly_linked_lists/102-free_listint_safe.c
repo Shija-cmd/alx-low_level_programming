64ef4f9cf1a43da49a284852bf2448ea3948b857
Shija-cmd <shijajuma@protonmail.com>
2023-11-21 00:01:00 +0000

    updated task 13 safe version

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -5,6 +5,7 @@ size_t free_listint_safe(listint_t **h);
 
 /**
  * looped_listint_count - Counts the number of unique nodes
+ *                      in a looped listint_t linked list.
  * @head: A pointer to the head of the listint_t to check.
  *
  * Return: If the list is not looped - 0.
@@ -57,7 +58,7 @@ size_t looped_listint_count(listint_t *head)
  *
  * Return: The size of the list that was freed.
  *
- * Description: Function sets the head to NULL.
+ * Description: The function sets the head to NULL.
  */
 size_t free_listint_safe(listint_t **h)
 {
